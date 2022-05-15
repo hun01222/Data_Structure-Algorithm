0157575a7eae54d88b2d392afacb3b60d7c12abf
박지훈 <hun01222@naver.com>
2022-05-15 14:18:09 +0000

    new

---
+++ disjoint_set_O(n).cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int parent[10];
+
+int find(int x){
+	if(x==parent[x]) return x;
+	return find(parent[x]);
+}
+
+void Union(int x, int y){
+	x=find(x);
+	y=find(y);
+	
+	if(x!=y)
+		parent[x]=y;
+}
+
+int main(){
+	for(int i=1; i<=10; i++)
+		parent[i]=i;
+	
+	int two_parent=find(2);
+	
+	Union(2, 3);
+	Union(3, 1);
+	
+	int new_two_parent=find(2);
+	cout << two_parent << ' ' << new_two_parent;
+}
